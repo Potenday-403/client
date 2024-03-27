bcd0fb3343de9cad38f07a7b22a292a2f955d677
tmdgus95 <8571053@naver.com>
2024-03-27 11:21:21 +0900

    배포시 환경변수
    use client 사용

--- src/components/LogoutTest.tsx
+++ src/components/LogoutTest.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 export function LogoutTest() {
   const handleClink = () => {
     const logoutURL = `https://kauth.kakao.com/oauth/logout?client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&logout_redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_ROGOUT_URI}`;
