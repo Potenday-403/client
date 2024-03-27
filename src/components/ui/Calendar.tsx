88cb5498415b52e6dce9d5ea0641b7fd7306b362
nonon <nononcrust@gmail.com>
2024-03-27 23:03:59 +0900

    Calendar 컴포넌트 추가

--- src/components/ui/Calendar.tsx
+++ src/components/ui/Calendar.tsx
@@ -3,78 +3,75 @@
 import { cn } from "@/utils/cn";
 import { differenceInCalendarDays } from "date-fns";
 import { ko } from "date-fns/locale";
-import { useEffect, useRef } from "react";
+import { createContext, useContext, useEffect, useRef } from "react";
 import {
   DateFormatter,
   DayPicker,
   DayProps,
-  Row,
-  RowProps,
   useDayRender,
 } from "react-day-picker";
 
-// 임시 타입
-export type Importance = "low" | "medium" | "high";
-
-export type Event = {
-  id: string;
-  title: string;
-  date: Date;
-  importance: Importance;
-};
+// TODO: collapse 기능 추가
+// TODO: 일정 표시 기능 추가
+// TODO: 선택된 날짜 표시 기능 추가
+// TODO: 아이콘 및 스타일 추가
 
 interface CalendarProps {
-  events: Event[];
+  selected?: Date;
   onSelect?: (date?: Date) => void;
-  isCollapsed?: boolean;
+  disablePastDates?: boolean;
 }
 
-export const Calendar = ({ events, onSelect, isCollapsed }: CalendarProps) => {
-  const getEventForDate = (events: Event[], date: Date) => {};
-
+export const Calendar = ({
+  selected,
+  onSelect,
+  disablePastDates = false,
+}: CalendarProps) => {
   return (
-    <DayPicker
-      locale={ko}
-      mode="single"
-      onSelect={onSelect}
-      id="date-picker"
-      showOutsideDays
-      fixedWeeks
-      formatters={{ formatCaption: formatCaption }}
-      components={{
-        Day: Day,
-      }}
-      classNames={{
-        months: "flex",
-        caption: "flex justify-center relative items-center h-10",
-        caption_label:
-          "font-semibold absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center",
-        nav: "flex items-center rounded-md w-full h-full justify-between border-b",
-        nav_button: "hover:bg-accents-1 rounded-full p-0.5 transition",
-        nav_button_previous: "!absolute left-2",
-        nav_button_next: "!absolute right-2",
-        head_row: "flex w-full",
-        head_cell:
-          "w-[50px] h-[38px] text-accents-2 font-normal m-0 flex items-center justify-center p-0",
-        row: "flex w-full mt-2 border-b",
-        cell: "relative rounded-md p-0 text-center focus-within:relative w-full h-[70px]",
-        day: cn(
-          "h-8 w-8 rounded-full p-0 text-center outline-none transition-all",
-          "hover:bg-accents-1",
-          "focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-opacity-50",
-        ),
-        day_today: "bg-red text-white hover:bg-red hover:text-white",
-        day_outside: "text-accents-4 aria-selected:text-white",
-        day_disabled: "text-accents-4 pointer-events-none bg-accents-5",
-        // day_range_middle:
-        // "aria-selected:!bg-ui-bg-highlight aria-selected:!text-ui-fg-interactive bg-red-500",
-        day_hidden: "invisible",
-      }}
-    />
+    <CalderContext.Provider value={{ disablePastDates }}>
+      <DayPicker
+        locale={ko}
+        mode="single"
+        selected={selected}
+        onSelect={onSelect}
+        id="date-picker"
+        showOutsideDays
+        fixedWeeks
+        formatters={{ formatCaption: formatCaption }}
+        components={{
+          Day: Day,
+        }}
+        classNames={{
+          months: "flex",
+          caption: "flex justify-center relative items-center h-10",
+          caption_label:
+            "font-semibold absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center",
+          nav: "flex items-center rounded-md w-full h-full justify-between border-b",
+          nav_button: "hover:bg-accents-1 rounded-full p-0.5 transition",
+          nav_button_previous: "!absolute left-2",
+          nav_button_next: "!absolute right-2",
+          head_row: "flex w-full",
+          head_cell:
+            "w-[50px] h-[38px] text-accents-2 font-normal m-0 flex items-center justify-center p-0",
+          row: "flex w-full mt-2 border-b",
+          cell: "relative rounded-md p-0 text-center focus-within:relative w-full h-[70px]",
+          day: cn(
+            "h-8 w-8 rounded-full p-0 text-center outline-none transition-all",
+            "hover:bg-accents-1",
+            "focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-opacity-50",
+          ),
+          day_today: "bg-red text-white hover:bg-red hover:text-white",
+          day_outside: "text-accents-4 aria-selected:text-white",
+          day_disabled: "text-accents-4 pointer-events-none bg-accents-5",
+          day_hidden: "invisible",
+        }}
+      />
+    </CalderContext.Provider>
   );
 };
 
 const Day = ({ date, displayMonth }: DayProps) => {
+  const { disablePastDates } = useCalendarContext();
   const ref = useRef<HTMLButtonElement>(null);
   const { activeModifiers, buttonProps, divProps, isButton } = useDayRender(
     date,
@@ -90,7 +87,7 @@ const Day = ({ date, displayMonth }: DayProps) => {
     }
   }, [selected]);
 
-  const disabled = isPastDate(date);
+  const disabled = isPastDate(date) && disablePastDates;
   const disabledStyle = "pointer-events-none text-accents-4";
 
   if (!isButton) {
@@ -118,23 +115,7 @@ const Day = ({ date, displayMonth }: DayProps) => {
       {...buttonPropsRest}
     >
       {buttonChildren}
-
-      {/* {today && <Dot />} */}
     </button>
-  );
-};
-
-const Dot = () => {
-  return (
-    <span
-      className={cn(
-        "absolute right-3 top-11 h-2 w-2 rounded-full bg-[#BBBBBB]",
-        // !selected && "bg-green-500",
-        // selected && "bg-red-500",
-        // selected && range_middle && "bg-purple-500",
-        // disabled && "bg-gray-500",
-      )}
-    />
   );
 };
 
@@ -149,4 +130,22 @@ const formatCaption: DateFormatter = (date) => {
   const formattedDate = `${year}년, ${month}월`;
 
   return formattedDate;
+};
+
+type CalendarContextValue = {
+  disablePastDates: boolean;
+};
+
+const CalderContext = createContext<CalendarContextValue | null>(null);
+
+const useCalendarContext = () => {
+  const context = useContext(CalderContext);
+
+  if (!context) {
+    throw new Error(
+      "useCalendarContext는 Calendar 컴포넌트 안에서만 사용할 수 있습니다.",
+    );
+  }
+
+  return context;
 };
