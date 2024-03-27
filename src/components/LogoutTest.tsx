030198a02e4d1357bb7d9d78ce60f4c3ee544d95
tmdgus95 <8571053@naver.com>
2024-03-27 11:16:44 +0900

    환경변수 확인
    로그아웃

--- src/components/LogoutTest.tsx
+++ src/components/LogoutTest.tsx
@@ -3,5 +3,8 @@ export function LogoutTest() {
     const logoutURL = `https://kauth.kakao.com/oauth/logout?client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&logout_redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_ROGOUT_URI}`;
     window.location.href = logoutURL;
   };
+  console.log(process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY);
+  console.log(process.env.NEXT_PUBLIC_KAKAO_ROGOUT_URI);
+
   return <button onClick={handleClink}>카카오와 함꼐 로그아웃</button>;
 }
