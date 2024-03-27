5f17adb5e512ae15ef10d069d3e6487270e07e9e
tmdgus95 <8571053@naver.com>
2024-03-27 11:45:05 +0900

    환경변수 테스트

--- src/app/auth/kakao/login/page.tsx
+++ src/app/auth/kakao/login/page.tsx
@@ -8,6 +8,10 @@ export default function KakaoLoginPage() {
   const searchParams = useSearchParams();
   const AuthorizationCode = searchParams.get("code")!;
 
+  console.log("로그아웃 테스트");
+  console.log(process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY, "레스트api");
+  console.log(process.env.NEXT_PUBLIC_KAKAO_ROGOUT_URI, "로그아웃 uri");
+
   const query = useQuery({
     queryKey: ["kakaotoken"],
     queryFn: () => getKakaoToken(AuthorizationCode),
--- src/components/LogoutTest.tsx
+++ src/components/LogoutTest.tsx
@@ -5,10 +5,10 @@ export function LogoutTest() {
     const logoutURL = `https://kauth.kakao.com/oauth/logout?client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&logout_redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_ROGOUT_URI}`;
     window.location.href = logoutURL;
   };
-  console.log("로그아웃 테스트");
 
+  console.log("로그아웃 테스트");
   console.log(process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY, "레스트api");
   console.log(process.env.NEXT_PUBLIC_KAKAO_ROGOUT_URI, "로그아웃 uri");
 
-  return <button onClick={handleClink}>카카오와 함꼐 로그아웃</button>;
+  return <button onClick={handleClink}>카카오와 함께 로그아웃</button>;
 }
