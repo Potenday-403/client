e5f6ddc194425effca6cb663b9266a1c960cccf5
nonon <nononcrust@gmail.com>
2024-03-27 23:09:34 +0900

    페이지 코드 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,9 @@
-import { EventCard } from "@/components/EventCard";
-import { Calendar, Event } from "@/components/ui/Calendar";
-
-const DUMMY_EVENTS: Event[] = [
-  {
-    id: "1",
-    title: "김밤비 결혼식",
-    date: new Date(),
-    importance: "low",
-  },
-  {
-    id: "2",
-    title: "김밤비 결혼식",
-    date: new Date(Date.now() - 24 * 60 * 60 * 1000),
-    importance: "medium",
-  },
-  {
-    id: "3",
-    title: "김밤비 결혼식",
-    date: new Date(Date.now() - 2 * 24 * 60 * 60 * 1000),
-    importance: "high",
-  },
-];
+import { Calendar } from "@/components/ui/Calendar";
 
 export default function HomePage() {
   return (
-    <div className="flex justify-center">
-      <main className="h-screen w-full max-w-[375px] border px-4 pt-8">
-        <Calendar events={DUMMY_EVENTS} />
-        <div className="mt-8 flex flex-col">
-          {DUMMY_EVENTS.map((event) => (
-            <EventCard key={event.id} {...event} />
-          ))}
-        </div>
-      </main>
-    </div>
+    <main className="px-4 pt-4">
+      <Calendar />
+    </main>
   );
 }
