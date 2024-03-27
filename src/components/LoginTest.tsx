c2d405951fe100f7aa98765588096c047cbb26d1
tmdgus95 <8571053@naver.com>
2024-03-27 11:10:02 +0900

    환경변수 확인
    KOE101 - 잘못된 앱 키 또는 앱 키 타입을 사용한 경우

--- src/components/LoginTest.tsx
+++ src/components/LoginTest.tsx
@@ -2,6 +2,9 @@
 
 export function LoginTest() {
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_ROGIN_URI}&response_type=code`;
+  console.log(process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY);
+  console.log(process.env.NEXT_PUBLIC_KAKAO_ROGIN_URI);
+
   const handleLogin = () => {
     window.location.href = kakaoURL;
   };
