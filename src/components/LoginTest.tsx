c33512d73a645c38e27fda26e46a63ac0f97df97
tmdgus95 <8571053@naver.com>
2024-03-27 11:26:20 +0900

    환경변수
    로그인 컴포넌트에서 확인

--- src/components/LoginTest.tsx
+++ src/components/LoginTest.tsx
@@ -4,6 +4,8 @@ export function LoginTest() {
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_ROGIN_URI}&response_type=code`;
   console.log(process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY);
   console.log(process.env.NEXT_PUBLIC_KAKAO_ROGIN_URI);
+  console.log(process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY);
+  console.log(process.env.NEXT_PUBLIC_KAKAO_ROGOUT_URI);
 
   const handleLogin = () => {
     window.location.href = kakaoURL;
