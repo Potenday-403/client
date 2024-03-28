c6bdc82dd5cfba0f46e550adac8bc43e5e945776
tmdgus95 <8571053@naver.com>
2024-03-28 10:33:17 +0900

    환경변수 테스트

--- src/app/auth/kakao/login/page.tsx
+++ src/app/auth/kakao/login/page.tsx
@@ -1,25 +1,34 @@
-"use client";
+// "use client";
 
-import { getKakaoToken } from "@/services/kakao";
-import { useQuery } from "@tanstack/react-query";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+// import { getKakaoToken } from "@/services/kakao";
+// import { useQuery } from "@tanstack/react-query";
+// import { useRouter, useSearchParams } from "next/navigation";
+// import { useEffect, useState } from "react";
 
-export default function KakaoLoginPage() {
-  const navigation = useRouter();
-  const searchParams = useSearchParams();
-  const AuthorizationCode = searchParams.get("code")!;
-  const [userInfo, setUserInfo] = useState();
+// export default function KakaoLoginPage() {
+//   const navigation = useRouter();
+//   const searchParams = useSearchParams();
+//   const AuthorizationCode = searchParams.get("code")!;
+//   const [userInfo, setUserInfo] = useState();
 
-  const query = useQuery({
-    queryKey: ["kakaotoken"],
-    queryFn: () => getKakaoToken(AuthorizationCode),
-  });
-  console.log(query.data, 1);
+//   const query = useQuery({
+//     queryKey: ["kakaotoken"],
+//     queryFn: () => getKakaoToken(AuthorizationCode),
+//   });
+//   console.log(query.data, 1);
 
-  useEffect(() => {
-    userInfo ? navigation.push("/") : navigation.push("/signup");
-  }, [userInfo]);
+//   useEffect(() => {
+//     userInfo ? navigation.push("/") : navigation.push("/signup");
+//   }, [userInfo]);
 
-  return <div></div>;
+//   return <div></div>;
+// }
+export default function Loginpage() {
+  console.log(1);
+  console.log(process.env.NEXT_PUBLIC_KAKAO_INTERGRITY);
+  console.log(process.env.NEXT_PUBLIC_KAKAO_ROGOUT_URI);
+  console.log(process.env.NEXT_PUBLIC_KAKAO_ROGIN_URI);
+  console.log(process.env.NEXT_PUBLIC_KAKAO_WEB_DOMAIN);
+
+  return <div>서버컴포넌트에서는 받아와 질지...</div>;
 }
