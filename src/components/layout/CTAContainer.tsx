ebcd854a8df102696b57bac5a1ed67e7306540a5
nonon <nononcrust@gmail.com>
2024-03-30 18:50:15 +0900

    CTAContainer 컴포넌트 수정

--- src/components/layout/CTAContainer.tsx
+++ src/components/layout/CTAContainer.tsx
@@ -1,22 +1,10 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 interface CTAContainerProps {
   children: React.ReactNode;
 }
 
 export const CTAContainer = ({ children }: CTAContainerProps) => {
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  if (!isMounted) {
-    return null;
-  }
-
   return (
     <div className="fixed bottom-0 left-0 right-0 mx-auto max-w-main p-4">
       {children}
